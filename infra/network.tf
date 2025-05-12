a7ad54fbb816f56bfbbebcc8300311fda0fb8aa2
twist35 <sabironantonin@outlook.fr>
2025-05-12 16:46:26 +0200

    network

--- infra/network.tf
+++ infra/network.tf
@@ -79,6 +79,10 @@ resource "aws_route_table_association" "public_subnet_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_route_table_association" "private_subnet_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
 
 resource "aws_nat_gateway" "ntg" {
   allocation_id = aws_eip.nat_eip.id
