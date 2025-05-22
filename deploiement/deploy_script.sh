22fdf9bf34e79fb16af4f684fbb566471201265a
twist35 <sabironantonin@outlook.fr>
2025-05-22 16:12:19 +0200

    maj deploy_srcipt

--- deploiement/deploy_script.sh
+++ deploiement/deploy_script.sh
@@ -8,7 +8,7 @@ echo "📚 Vérification/installation des collections Ansible ..."
 ansible-galaxy collection install -r requirements.yml #--force
 
 echo "🔗 Test de connexion aux machines distantes..."
-ansible -i hosts.yml all -m ping --ssh-common-args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+ansible -i inventory/hosts.yml all -m ping --ssh-common-args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
 
  echo "📦 Lancement de tout le playbook..."
