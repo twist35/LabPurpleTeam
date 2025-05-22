d6cf04f4b87c97403da74d2ddc39479e83eb55f3
twist35 <sabironantonin@outlook.fr>
2025-05-22 18:25:43 +0200

    eu-west-3

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "backend-antonin-tf2"
+    bucket  = "backend-antonin-tf"
     key     = "ProjetM2/terraform.tfstate"
     dynamodb_table = "terraform-lock"
     region  = "eu-west-3"
