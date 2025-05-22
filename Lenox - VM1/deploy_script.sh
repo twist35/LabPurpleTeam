464fb869f9a5ad2acd0723145a3ef56dcbd7c364
marius2468 <marius.fremond@isen-ouest.yncrea.fr>
2025-05-22 10:14:06 +0200

    Add my key to the authorized_keys

--- Lenox - VM1/deploy_script.sh
+++ Lenox - VM1/deploy_script.sh
@@ -5,10 +5,10 @@ set -e
 echo "🚀 Deploy Lenox machine with ansible"
 
 echo "🔗 Test remote connexion..."
-ansible -i ansible/hosts.ini lenox -m ping
+ansible -i ansible/hosts.yaml lenox -m ping
 
 echo "📦 Launch playbook..."
-ansible-playbook -i ansible/hosts.ini ansible/playbook.yml
+ansible-playbook -i ansible/hosts.yaml ansible/playbook.yml
 
 echo "✅ Deploy end with success !"
 
