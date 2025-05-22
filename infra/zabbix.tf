f371d62825a919b1e84bd4bd97ab983f651d610f
GuillaumeRobin9 <guirobin37@gmail.com>
2025-05-22 11:14:35 +0200

    I don't remember what the fuck I did

--- infra/zabbix.tf
+++ infra/zabbix.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "zabbix" {
-  ami                         = "ami-0644165ab979df02d" #Debian 12 ARM ami-0639bd0dd196bc480
-  instance_type               = "t3.large"
+  ami                         = "ami-007891ae041e0263b" #Debian 12 ARM ami-0639bd0dd196bc480
+  instance_type               = "t3.medium"
   key_name                    = aws_key_pair.vm_key.key_name
   subnet_id                   = aws_subnet.private.id
   private_ip                  = "10.0.1.25" # IP privée statique 
