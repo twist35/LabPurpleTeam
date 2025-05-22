e29aab7c78b7698c275161281e5b2439c860b0de
twist35 <sabironantonin@outlook.fr>
2025-05-22 16:54:50 +0200

    script

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket  = "backend-antonin-tf"
     key     = "ProjetM2/terraform.tfstate"
     dynamodb_table = "terraform-lock"
-    region  = "eu-west-3"
+    region  = "eu-west-1"
     encrypt = true
   }
 }
--- launch.sh
+++ launch.sh
@@ -3,6 +3,40 @@ set -e
 
 echo "🚀 Lancement de Terraform dans le dossier infra/ ..."
 cd infra/
+
+#!/bin/bash
+set -e
+
+echo "🚀 Lancement de Terraform dans le dossier infra/ ..."
+cd infra/
+
+# 🔍 Init seulement si nécessaire
+if [ ! -d ".terraform" ] || [ ! -f ".terraform.lock.hcl" ]; then
+  echo "🛠️  Initialisation de Terraform (init)..."
+  terraform init
+else
+  echo "✅ Terraform déjà initialisé, init non nécessaire."
+fi
+
+echo "📦 Application de Terraform..."
+terraform apply -auto-approve
+
+echo "🌐 Récupération de l'IP du bastion..."
+BASTION_IP=$(terraform output -raw bastion_ip)
+
+cd ..
+
+GROUP_VARS_FILE="deploiement/inventory/group_vars/all.yml"
+
+echo "📝 Mise à jour de $GROUP_VARS_FILE avec bastion_ip=$BASTION_IP ..."
+sed -i "s/^bastion_ip: .*/bastion_ip: ${BASTION_IP}/" "$GROUP_VARS_FILE"
+
+echo "✅ Résultat final :"
+grep bastion_ip "$GROUP_VARS_FILE"
+
+echo "📦 Lancement du playbook Ansible ..."
+ansible-playbook -i deploiement/inventory/ playbook.yml
+
 terraform apply -auto-approve
 
 echo "🌐 Récupération de l'IP du bastion..."
